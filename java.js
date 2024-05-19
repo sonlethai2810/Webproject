d33156fafecf847c15d6c3b21d57ae15d3b96e39
DamVietHung <22014569@st.phenikaa-uni.edu.vn>
2024-05-20 05:56:28 +0700

    goi y search bả chua xong :)

--- java.js
+++ java.js
@@ -1,3 +1,26 @@
 document.getElementById('loginLink').addEventListener('click', function() {
     window.open('login.html', '_blank', 'width=400,height=400');
 });
+
+const suggestions = ["Thuật toán sắp xếp ", "Bubble Sort", "Insertion Sort", "Selection Sort", "Merge Sort ", "Quick Sort", "Shell Sort"];
+
+function showSuggestions(value) {
+    const suggestionsBox = document.getElementById('suggestions-box');
+    suggestionsBox.innerHTML = '';
+    if (value.length === 0) {
+        return;
+    }
+    const filteredSuggestions = suggestions.filter(suggestion => 
+        suggestion.toLowerCase().includes(value.toLowerCase())
+    );
+    filteredSuggestions.forEach(suggestion => {
+        const suggestionItem = document.createElement('div');
+        suggestionItem.classList.add('suggestion-item');
+        suggestionItem.textContent = suggestion;
+        suggestionItem.onclick = () => {
+            document.getElementById('search-box').value = suggestion;
+            suggestionsBox.innerHTML = '';
+        };
+        suggestionsBox.appendChild(suggestionItem);
+    });
+}
